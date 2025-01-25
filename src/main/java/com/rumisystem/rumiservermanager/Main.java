8071f6e88020e64a78634c1a088fe68aa1954824
SINtyanneru <irjitgd@gmail.com>
2025-01-25 18:35:54 +0900

    基本的な接続処理のみ

--- src/main/java/com/rumisystem/rumiservermanager/Main.java
+++ src/main/java/com/rumisystem/rumiservermanager/Main.java
@@ -4,56 +4,79 @@ import static com.rumisystem.rumi_java_lib.LOG_PRINT.Main.LOG;
 
 import java.util.Iterator;
 
+import com.rumisystem.rumi_java_lib.ArrayNode;
+import com.rumisystem.rumi_java_lib.CONFIG;
+import com.rumisystem.rumi_java_lib.EXCEPTION_READER;
 import com.rumisystem.rumi_java_lib.LOG_PRINT.LOG_TYPE;
 import com.rumisystem.rumiservermanager.PARENT.ParentMain;
 import com.rumisystem.rumiservermanager.TYPE.START_MODE;
 
 public class Main {
 	public static final String VERSION = "0.5";
+	public static ArrayNode CONFIG_DATA = null;
 
 	public static void main(String[] args) {
-		START_MODE MODE = null;
+		try {
+			START_MODE MODE = null;
 
-		LOG(LOG_TYPE.INFO, "---------------[RumiServerManager]---------------");
-		LOG(LOG_TYPE.INFO, "るみ");
-		LOG(LOG_TYPE.INFO, "Version:" + VERSION);
-		LOG(LOG_TYPE.INFO, "-------------------------------------------------");
+			LOG(LOG_TYPE.INFO, "---------------[RumiServerManager]---------------");
+			LOG(LOG_TYPE.INFO, "るみ");
+			LOG(LOG_TYPE.INFO, "Version:" + VERSION);
+			LOG(LOG_TYPE.INFO, "-------------------------------------------------");
 
-		for (int I = 0; I < args.length; I += 2) {
-			String KEY = args[I];
-			String VAL = args[I + 1];
-			
-			switch (KEY) {
-				case "--mode": {
-					if (VAL.equals("parent")) {
-						MODE = START_MODE.PARENT;
-					} else if (VAL.equals("child")) {
-						MODE = START_MODE.CHILD;
-					} else {
-						System.err.println("エラー:モード選択が意味不明です");
+			//設定をロード
+			CONFIG_DATA = new CONFIG().DATA;
+			if (CONFIG_DATA == null) {
+				LOG(LOG_TYPE.FAILED, "設定ファイルが読み込めませんでした");
+				System.exit(1);
+			}
+
+			//コマンドライン引数を解析
+			for (int I = 0; I < args.length; I += 2) {
+				String KEY = args[I];
+				String VAL = args[I + 1];
+				
+				switch (KEY) {
+					case "--mode": {
+						if (VAL.equals("parent")) {
+							MODE = START_MODE.PARENT;
+						} else if (VAL.equals("child")) {
+							MODE = START_MODE.CHILD;
+						} else {
+							System.err.println("エラー:モード選択が意味不明です");
+							System.exit(1);
+						}
+						break;
+					}
+				
+					default: {
+						System.err.println("エラー:コマンドライン引数が意味不明です");
 						System.exit(1);
 					}
-					break;
-				}
-			
-				default: {
-					System.err.println("エラー:コマンドライン引数が意味不明です");
-					System.exit(1);
 				}
 			}
-		}
 
-		if (MODE != null) {
-			if (MODE == START_MODE.PARENT) {
-				//親
-				new ParentMain().Main();
+			if (MODE != null) {
+				if (MODE == START_MODE.PARENT) {
+					//親
+					new ParentMain().Main();
+				} else {
+					//子
+				}
 			} else {
-				//子
+				System.out.println("コマンドライン引数に親か子かをセットしてください!");
+				System.out.println("親:parent");
+				System.out.println("子:child");
 			}
-		} else {
-			System.out.println("コマンドライン引数に親か子かをセットしてください!");
-			System.out.println("親:parent");
-			System.out.println("子:child");
+		} catch (Exception EX) {
+			EXP(EX);
+		}
+	}
+
+	public static void EXP(Exception EX) {
+		String EX_TEXT = EXCEPTION_READER.READ(EX);
+		for (String LINE:EX_TEXT.split("\n")) {
+			LOG(LOG_TYPE.FAILED, LINE);
 		}
 	}
 }
--- src/main/java/com/rumisystem/rumiservermanager/PARENT/ParentMain.java
+++ src/main/java/com/rumisystem/rumiservermanager/PARENT/ParentMain.java
@@ -1,7 +1,79 @@
 package com.rumisystem.rumiservermanager.PARENT;
 
+import static com.rumisystem.rumi_java_lib.LOG_PRINT.Main.LOG;
+import static com.rumisystem.rumiservermanager.Main.CONFIG_DATA;
+import static com.rumisystem.rumiservermanager.Main.VERSION;
+import static com.rumisystem.rumiservermanager.Main.EXP;
+
+import java.io.IOException;
+
+import com.rumisystem.rumi_java_lib.LOG_PRINT.LOG_TYPE;
+import com.rumisystem.rumi_java_lib.Socket.Server.SocketServer;
+import com.rumisystem.rumi_java_lib.Socket.Server.CONNECT_EVENT.CONNECT_EVENT;
+import com.rumisystem.rumi_java_lib.Socket.Server.CONNECT_EVENT.CONNECT_EVENT_LISTENER;
+import com.rumisystem.rumi_java_lib.Socket.Server.EVENT.CloseEvent;
+import com.rumisystem.rumi_java_lib.Socket.Server.EVENT.EVENT_LISTENER;
+import com.rumisystem.rumi_java_lib.Socket.Server.EVENT.MessageEvent;
+import com.rumisystem.rumi_java_lib.Socket.Server.EVENT.ReceiveEvent;
+
 public class ParentMain {
-	public void Main() {
-		
+	public void Main() throws IOException {
+		SocketServer SS = new SocketServer();
+		SS.setEventListener(new CONNECT_EVENT_LISTENER() {
+			@Override
+			public void CONNECT(CONNECT_EVENT SESSION) {
+				try {
+					String IP = SESSION.getIP();
+					boolean[] HELO = {false}; //←javaのクソ仕様
+
+					//ログ
+					LOG(LOG_TYPE.OK, "Connect:" + IP);
+
+					//ようこそメッセージ
+					SESSION.sendMessage("SALUTON " + VERSION + "\r\n");
+
+					SESSION.setEventListener(new EVENT_LISTENER() {
+						@Override
+						public void Receive(ReceiveEvent e) {
+							try {
+								String[] CMD = e.getString().split(" ");
+
+								if (HELO[0]) {
+									switch (CMD[0]) {
+										default: {
+											SESSION.sendMessage("400 Command ga nai\r\n");
+											break;
+										}
+									}
+								} else {
+									//接続後の初期設定を行う必要がある
+									if (CMD[0].equals("HELO")) {
+										HELO[0] = true;
+										SESSION.sendMessage("200 OK\r\n");
+									} else {
+										//最初に挨拶をしないのは99%別のプロトコルを送ってきているおバカさんなので切断する
+										SESSION.close();
+									}
+								}
+							} catch (Exception EX) {
+								EXP(EX);
+							}
+						}
+
+						@Override
+						public void Message(MessageEvent e) {
+						}
+
+						@Override
+						public void Close(CloseEvent e) {
+							LOG(LOG_TYPE.OK, "Disconnect:" + IP);
+						}
+					});
+				} catch (Exception EX) {
+					EXP(EX);
+				}
+			}
+		});
+		SS.START(CONFIG_DATA.get("RMS").asInt("PORT"));
 	}
 }
